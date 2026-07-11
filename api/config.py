c994af7c7321eec9d1bc1ecc0a19360dd77bedbb
Joe Strommen <joe.strommen@gmail.com>
2026-07-11 13:14:43 -0500

    Added localhost:4173 to CORS

--- api/config.py
+++ api/config.py
@@ -10,6 +10,8 @@ CORS_ORIGINS = [
     "https://web-backend.electricity.works",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:4173",
+    "http://127.0.0.1:4173",
 ]
 
 JOURNAL_DB_USER = "journaldb"
